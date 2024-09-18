b98d45e01f7152bd271a0b68440256f729d89c27
Franziska Scheer <franzi-scheer@web.de>
2024-09-18 17:40:33 +0200

    2nd refactoring

--- src/viking.js
+++ src/viking.js
@@ -51,33 +51,12 @@ class War {
   addSaxon(saxon) {
     this.saxonArmy.push(saxon);
   }
-    vikingAttack() {
-      //select viking and saxon
-      const attackingViking =
-        this.vikingArmy[parseInt(Math.random() * this.vikingArmy.length)];
-      const attackedSaxon =
-        this.saxonArmy[parseInt(Math.random() * this.saxonArmy.length)];
-      //add damage to saxon with strength of viking
-      const attack = attackedSaxon.receiveDamage(attackingViking.attack());
-      //remove saxon if dead
-      if (attackedSaxon.health <= 0) {
-        const index = this.saxonArmy.indexOf(attackedSaxon);
-        this.saxonArmy.splice(index, 1);
-      }
-      return attack;
-    }
-    saxonAttack() {
-      const attackingSaxon =
-        this.saxonArmy[parseInt(Math.random() * this.saxonArmy.length)];
-      const attackedViking =
-        this.vikingArmy[parseInt(Math.random() * this.vikingArmy.length)];
-      const attack = attackedViking.receiveDamage(attackingSaxon.attack());
-      if (attackedViking.health <= 0) {
-        const index = this.vikingArmy.indexOf(attackedViking);
-        this.vikingArmy.splice(index, 1);
-      }
-      return attack;
-    }
+  vikingAttack() {
+    return this.genericAttack(this.saxonArmy, this.vikingArmy);
+  }
+  saxonAttack() {
+    return this.genericAttack(this.vikingArmy, this.saxonArmy);
+  }
 
   //Iteration 5
   showStatus() {
@@ -91,7 +70,7 @@ class War {
     }
   }
   //refactoring
-  armyAttack(attackingArmy, attackedArmy) {
+  genericAttack(attackingArmy, attackedArmy) {
     const attackingSoldier =
       attackingArmy[Math.floor(Math.random() * attackingArmy.length)];
     const attackedSoldier =
